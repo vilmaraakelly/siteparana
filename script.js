7d5d6310e7a65457519976d884fa20cb8ca1f4a2
vilmaraakelly <vilmaraakelly@gmail.com>
2026-05-20 23:03:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,25 @@ const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
 
-  elements.forEach(el => {
+  // Efeito Fade
+  elements.forEach((el) => {
 
     const top = el.getBoundingClientRect().top;
 
-    if(top < window.innerHeight - 100){
+    if (top < window.innerHeight - 100) {
       el.classList.add('show');
     }
 
   });
 
-  if(window.scrollY > 50){
+  // Mudança no Header
+  if (window.scrollY > 50) {
+
     header.classList.add('scrolled');
+
   } else {
+
     header.classList.remove('scrolled');
-  }
-
-});
-
-const audioFile = document.getElementById('audioFile');
-const player = document.getElementById('player');
-
-audioFile.addEventListener('change', function(){
-
-  const arquivo = this.files[0];
-
-  if(arquivo){
-
-    const url = URL.createObjectURL(arquivo);
-
-    player.src = url;
 
   }
 
